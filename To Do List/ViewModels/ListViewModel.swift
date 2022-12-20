c8c7913e0fe59442c32d9b9d6f088bdd83c35c8d
OlliGN <Olga199411@gmail.com>
2022-12-20 11:32:33 -0600

    Save and persist data with UserDefaults

--- To Do List/ViewModels/ListViewModel.swift
+++ To Do List/ViewModels/ListViewModel.swift
@@ -7,20 +7,44 @@
 
 import Foundation
 
+/*
+ CRUD FUNCTIONS
+ 
+ CREATE
+ READ
+ UPDATE
+ DELETE
+ 
+ */
+
 class ListViewModel: ObservableObject {
-    @Published var items:[ItemModel] = []
+    @Published var items:[ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey:String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title", isCompleted: false),
-            ItemModel(title: "This is the second title", isCompleted: true),
-            ItemModel(title: "This is the thirs title", isCompleted: false),
-        ]
-        items.append(contentsOf: newItems)
+        
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
+        
+//        let newItems = [
+//            ItemModel(title: "This is the first title", isCompleted: false),
+//            ItemModel(title: "This is the second title", isCompleted: true),
+//            ItemModel(title: "This is the thirs title", isCompleted: false),
+//        ]
+//        items.append(contentsOf: newItems)
     }
     func deleteItem(indexSet:IndexSet) {
         items.remove(atOffsets: indexSet)
@@ -47,6 +71,11 @@ class ListViewModel: ObservableObject {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             //run this
             items[index] = item.updateCompletion()
+        }
+    }
+    func saveItems() {
+        if let encodedData = try?  JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
--- To Do List/Views/ListView.swift
+++ To Do List/Views/ListView.swift
@@ -16,7 +16,7 @@ struct ListView: View {
                 ListRowView(item: item)
                     .onTapGesture {
                         withAnimation(.linear) {
-                            
+                            listViewModel.updateItem(item: item)
                         }
                     }
                 
